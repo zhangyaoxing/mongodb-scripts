23170689d755cf0c7f367fab460f686dfbcebecd
Yaoxing <yaoxing.zhang@gmail.com>
2016-09-20 14:53:45 +0800

    Update switch-automation-agent-user.sh
    missing one directory /var/lib/mongodb-mms-automation/

--- switch-automation-agent-user.sh
+++ switch-automation-agent-user.sh
@@ -4,6 +4,7 @@ service mongodb-mms-automation-agent stop
 chown -R $SERVICE_USER:$SERVICE_GROUP /data/
 chown -R $SERVICE_USER:$SERVICE_GROUP /var/log/mongodb-mms-automation/
 chown -R $SERVICE_USER:$SERVICE_GROUP /etc/mongodb-mms/
+chown -R $SERVICE_USER:$SERVICE_GROUP /var/lib/mongodb-mms-automation/
 sed -i 's/SERVICE_USER="mongod"/SERVICE_USER="'$SERVICE_USER'"\nSERVICE_GROUP="'$SERVICE_GROUP'"/g' /etc/init.d/mongodb-mms-automation-agent
 sed -i 's/-g $SERVICE_USER/-g $SERVICE_GROUP/g' /etc/init.d/mongodb-mms-automation-agent
 service mongodb-mms-automation-agent start
