5dcbf9ba5a89c750c3659e09a60e33165d08d5bf
Yaoxing <yaoxing.zhang@gmail.com>
2016-11-07 17:29:26 +0800

    - output backup of documents to be deleted

--- ttl/ttl.js
+++ ttl/ttl.js
@@ -1,7 +1,7 @@
-var DAYS_TO_KEEP = 130;
+var DAYS_TO_KEEP = 136;
 var COLLECTION = "IOC_LSPLocInfo";
-var GAP_TIME_SECS = 10;
-var DELETE_BATCH_SIZE = 1;
+var GAP_TIME_SECS = 1;
+var DELETE_BATCH_SIZE = 100;
 
 var secsToKeep = DAYS_TO_KEEP * 86400;	// 60m * 60s * 24h = 86400
 var now = new Date().getTime();
@@ -10,25 +10,20 @@ var objId = ObjectId(hexSecs + "0000000000000000");
 
 var ttlRemove = function() {
 	var deletePoint = null;
-	var deletePointResult = db[COLLECTION].find({_id: {$lte: objId}}, {_id: 1})
+	// find the batch of documents to delete
+	var deletePointResult = db[COLLECTION].find({_id: {$lte: objId}})
 		.sort({_id: 1})
-		.skip(DELETE_BATCH_SIZE)
-		.limit(1)
+		.limit(DELETE_BATCH_SIZE)
 		.toArray();
 	if (deletePointResult.length) {
-		var deletePoint = deletePointResult[0]._id;
-	} else {
-		deletePointResult = db[COLLECTION].find({_id: {$lte: objId}}, {_id: 1})
-			.sort({_id: -1})
-			.limit(1)
-			.toArray();
-		if (deletePointResult.length) {
-			deletePoint = deletePointResult[0]._id;
-		}
+		var deletePoint = deletePointResult[deletePointResult.length - 1]._id;
 	}
 
 	if (deletePoint) {
-		print(deletePoint.getTimestamp());
+		for (var i = deletePointResult.length - 1; i >= 0; i--) {
+			printjsononeline(deletePointResult[i]);
+		}
+		// print(deletePoint.getTimestamp());
 		db[COLLECTION].remove({_id: {$lt: deletePoint}});
 		return true;
 	}
